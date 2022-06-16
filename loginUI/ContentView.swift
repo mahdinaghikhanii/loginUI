3e8afc25b68b40eb04ea52758b43032fbaa412d8
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-06-16 20:50:08 +0430

    Initial Commit

---
+++ loginUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  loginUI
+//
+//  Created by mahdi naghikhani on 16/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ loginUI/loginUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginUIApp.swift
+//  loginUI
+//
+//  Created by mahdi naghikhani on 16/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct loginUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
